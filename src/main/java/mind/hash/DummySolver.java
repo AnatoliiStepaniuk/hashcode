05bb52bf52dcc96ccf7b5654f71669c03e84ed57
scadge <scadgek@live.com>
2018-03-01 22:01:41 +0200

    test solution

---
+++ src/main/java/mind/hash/DummySolver.java
@@ -0,0 +1,14 @@
+package mind.hash;
+
+import mind.hash.model.Car;
+
+import java.util.List;
+
+public class DummySolver implements Solver
+{
+  @Override
+  public List<Car> solve()
+  {
+    return null;
+  }
+}
