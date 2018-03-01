2ef71419067e34a4be125087fb17c44a42cef0ff
Бондарь Сергей <bondar.s1995@gmail.com>
2018-03-01 21:46:49 +0200

    optimizer

---
+++ src/main/java/mind/hash/model/Optimizer.java
@@ -0,0 +1,71 @@
+package mind.hash.model;
+
+import mind.hash.model.Position;
+import mind.hash.model.Ride;
+
+import java.util.List;
+import java.util.TreeSet;
+import java.util.stream.Collectors;
+
+public class Optimizer {
+    public static final Position START = new Position(0, 0);
+
+    TreeSet<Ride> getSortedRides(
+            Position currentPosition,
+            List<Ride> rides,
+            final int currentStep,
+            final int stepsLeft
+    ) {
+        if (currentPosition == null) {
+            currentPosition = START;
+        }
+        Position finalStart = currentPosition;
+        TreeSet<Ride> sortedRides = new TreeSet<>(
+                (r1, r2) -> {
+                    int scoreRide1 = finalStart.distanceTo(r1.start);
+                    int scoreRide2 = finalStart.distanceTo(r2.start);
+                    scoreRide1 += currentStep - r1.earliestStart;
+                    scoreRide2 += currentStep - r2.earliestStart;
+                    scoreRide1 -= r1.getLength();
+                    scoreRide2 -= r2.getLength();
+                    return Integer.compare(scoreRide2, scoreRide1);
+                }
+        );
+        sortedRides.addAll(
+                rides.stream().filter(
+                        ride -> ride.latestFinish < currentStep || ride.getLength() < stepsLeft
+                ).collect(Collectors.toList())
+        );
+        return sortedRides;
+    }
+
+//    List<RidePath> getPerspectivePath(TreeSet<Ride> sortedRides, int steps, int cars) {
+//        List<RidePath> perspectivePath = new ArrayList<>();
+//        int i = 0;
+//        for (Ride ride: sortedRides) {
+//            if (i == cars) {
+//                break;
+//            } else {
+//                // removing perspective ride
+//                sortedRides.remove(ride);
+//                perspectivePath.add(new RidePath(ride, null, null));
+//            }
+//        }
+//        return perspectivePath;
+//    }
+//
+//    List<RidePath> createPath(List<Ride> perspectivePath, TreeSet<Ride> sortedRides, int steps, int cars) {
+//        List<RidePath> perspectivePath = new ArrayList<>();
+//        int i = 0;
+//        for (Ride ride: sortedRides) {
+//            if (i == cars) {
+//                break;
+//            } else {
+//                // removing perspective ride
+//                sortedRides.remove(ride);
+//                perspectivePath.add(new RidePath(ride, null, null));
+//            }
+//        }
+//        return perspectivePath;
+//    }
+}
--- src/main/java/mind/hash/model/Ride.java
+++ src/main/java/mind/hash/model/Ride.java
@@ -1,11 +1,11 @@
 package mind.hash.model;
 
 public class Ride {
-    Position start;
-    Position finish;
-    int earliestStart;
-    int latestFinish;
-    int id;
+    public Position start;
+    public Position finish;
+    public int earliestStart;
+    public int latestFinish;
+    public int id;
 
     public Ride(Position start, Position finish, int earliestStart, int latestFinish, int id) {
         this.start = start;
@@ -17,6 +17,30 @@ public class Ride {
 
     public int getLength() {
         return start.distanceTo( finish );
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Ride ride = (Ride) o;
+
+        if (earliestStart != ride.earliestStart) return false;
+        if (latestFinish != ride.latestFinish) return false;
+        if (id != ride.id) return false;
+        if (start != null ? !start.equals(ride.start) : ride.start != null) return false;
+        return finish != null ? finish.equals(ride.finish) : ride.finish == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = start != null ? start.hashCode() : 0;
+        result = 31 * result + (finish != null ? finish.hashCode() : 0);
+        result = 31 * result + earliestStart;
+        result = 31 * result + latestFinish;
+        result = 31 * result + id;
+        return result;
     }
 
     @Override
