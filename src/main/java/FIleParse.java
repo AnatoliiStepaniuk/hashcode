199fd8e6dc57e8d60e69eaf4a6e068ae61659561
Denys Konakhevych <koxa29@gmail.com>
2018-03-01 20:36:01 +0200

    changed java version

--- src/main/java/FIleParse.java
+++ src/main/java/FIleParse.java
@@ -1,9 +1,27 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
 /**
  * Created by Denys on 2018-03-01.
  */
 public class FIleParse {
 
     public void parse(String fileName) {
+        ClassLoader classLoader = getClass().getClassLoader();
+        File file = new File(classLoader.getResource(fileName).getFile());
 
+        try (Scanner scanner = new Scanner(file)) {
+
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                result.append(line).append("\n");
+            }
+
+            scanner.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
