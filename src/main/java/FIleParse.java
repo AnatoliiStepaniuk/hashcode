dbb4f37dc83673d978d65c38fcb083190dfc5016
Denys Konakhevych <koxa29@gmail.com>
2018-03-01 21:09:40 +0200

    solving

--- src/main/java/FIleParse.java
+++ src/main/java/FIleParse.java
@@ -1,3 +1,7 @@
+import model.Position;
+import model.Problem;
+import model.Ride;
+
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -9,28 +13,48 @@ import java.util.Scanner;
  */
 public class FIleParse {
 
-    public void parse(String fileName) {
+    public Problem parse(String fileName) {
         ClassLoader classLoader = getClass().getClassLoader();
         File file = new File(classLoader.getResource(fileName).getFile());
 
-        boolean headerParsed = false;
-        List<Object> rides = new ArrayList<>();
+        Problem problem = null;
+        int rideId = 0;
         try (Scanner scanner = new Scanner(file)) {
             while (scanner.hasNextLine()) {
-                if (!headerParsed) {
-
+                if (problem == null) {
+                    problem = parseProblem(scanner.nextLine());
+                    continue;
                 }
                 String row = scanner.nextLine();
-                rides.add(parseRide(row));
+                problem.getRides().add(parseRide(rideId, row));
+                rideId++;
             }
-            scanner.close();
-
+            return problem;
         } catch (IOException e) {
             e.printStackTrace();
+            return null;
         }
     }
 
-    private Object parseRide(String row) {
+    private Problem parseProblem(String header) {
+        String[] problemParts = header.split(" ");
+        return new Problem()
+                .setRows(Integer.parseInt(problemParts[0]))
+                .setColumns(Integer.parseInt(problemParts[1]))
+                .setVehicles(Integer.parseInt(problemParts[2]))
+                .setRides(new ArrayList<>(Integer.parseInt(problemParts[3])))
+                .setBonus(Integer.parseInt(problemParts[4]))
+                .setSteps(Integer.parseInt(problemParts[5]));
+    }
+
+    private Ride parseRide(int id, String row) {
         String[] ride = row.split(" ");
+        return new Ride(
+                new Position(Integer.parseInt(ride[0]), Integer.parseInt(ride[1])),
+                new Position(Integer.parseInt(ride[2]), Integer.parseInt(ride[3])),
+                Integer.parseInt(ride[4]),
+                Integer.parseInt(ride[5]),
+                id
+        );
     }
 }
