604f12bb73f8b263a92f93ae382da62b7f96ade6
anatolii.stepaniuk <anatolii.stepaniuk@gmail.com>
2018-03-01 21:36:55 +0200

    Solver interface

---
+++ src/main/java/GreedySolver.java
@@ -0,0 +1,35 @@
+import mind.hash.Solver;
+import mind.hash.model.Car;
+import mind.hash.model.Ride;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GreedySolver implements Solver {
+
+    private final List<Car> cars;
+    private final List<Ride> rides;
+
+    public GreedySolver(List<Car> cars, List<Ride> rides) {
+        this.cars = cars;
+        this.rides = rides;
+    }
+
+    public List<Car> solve(){
+        List<Car> carsWithAssignedRides = new ArrayList(cars);
+
+        for(Ride ride : rides){
+
+            for(Car car : carsWithAssignedRides){
+                if(car.canFulfill(ride)){
+                    car.add(ride);
+                    break;
+                }
+            }
+
+        }
+
+        return carsWithAssignedRides;
+    }
+
+}
---
+++ src/main/java/mind/hash/Solver.java
@@ -0,0 +1,10 @@
+package mind.hash;
+
+import mind.hash.model.Car;
+
+import java.util.List;
+
+public interface Solver {
+
+    List<Car> solve();
+}
