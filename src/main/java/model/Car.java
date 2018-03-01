f9e3f83d0e9ab559bdf830423b7800bc14da7a48
scadge <scadgek@live.com>
2018-03-01 21:11:33 +0200

    total score calculation

--- src/main/java/model/Car.java
+++ src/main/java/model/Car.java
@@ -1,5 +1,6 @@
 package model;
 
+import java.util.Comparator;
 import java.util.List;
 
 public class Car {
@@ -14,4 +15,26 @@ public class Car {
 //        int startTime = acceptedRideList.isEmpty() ?
         return true;
     }
+
+  public int getScore()
+  {
+    Position current = new Position( 0, 0 );
+    int currentStep = 0;
+
+    acceptedRideList.sort( Comparator.comparingInt( ride -> ride.actualStep ) );
+
+    int score = 0;
+    for( AcceptedRide acceptedRide : acceptedRideList )
+    {
+      score += acceptedRide.getLength();
+
+      int stepsToStart = current.distanceTo( acceptedRide.start );
+      if( (currentStep += stepsToStart) <= acceptedRide.earliestStart )
+      {
+        score += new Problem().bonus;
+      }
+    }
+
+    return score;
+  }
 }
